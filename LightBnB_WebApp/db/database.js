481793537a253d03ea7c2ee7e94e2354d534926d
JT Smith <nexxai@gmail.com>
2023-09-21 12:21:23 -0600

    Removing unnecessary comments

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -29,14 +29,6 @@ const getUserWithEmail = function (email) {
       console.log(err.message);
       return Promise.resolve(null);
     });
-  // let resolvedUser = null;
-  // for (const userId in users) {
-  //   const user = users[userId];
-  //   if (user && user.email.toLowerCase() === email.toLowerCase()) {
-  //     resolvedUser = user;
-  //   }
-  // }
-  // return Promise.resolve(resolvedUser);
 };
 
 /**
@@ -63,11 +55,6 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  // const userId = Object.keys(users).length + 1;
-  // user.id = userId;
-  // users[userId] = user;
-  // return Promise.resolve(user);
-
   return pool
     .query(
       `INSERT INTO users (name, password, email) VALUES ($1, $2, $3) RETURNING *;`,
